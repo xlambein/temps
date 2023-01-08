0055bf52a698b9cef9b2228019a34146865b42e3
Xavier Lambein <xlambein@gmail.com>
2023-01-08 11:42:03 +0100

    Switch nix build to naersk

--- flake.nix
+++ flake.nix
@@ -1,75 +1,61 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    crate2nix = {
-      url = "github:kolloch/crate2nix";
-      flake = false;
-    };
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
   };
 
-  outputs = { self, nixpkgs, utils, rust-overlay, crate2nix, ... }:
-    let
-      name = "temps";
-    in
-    utils.lib.eachDefaultSystem
-      (system:
-        let
-          # Imports
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              rust-overlay.overlay
-              (self: super: {
-                # Because rust-overlay bundles multiple rust packages into one
-                # derivation, specify that mega-bundle here, so that crate2nix
-                # will use them automatically.
-                rustc = self.rust-bin.stable.latest.default;
-                cargo = self.rust-bin.stable.latest.default;
-              })
-            ];
-          };
-          inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
-            generatedCargoNix;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    naersk,
+    ...
+  }: let
+    name = "temps";
+  in
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [rust-overlay.overlays.default];
+        };
 
-          # Create the cargo2nix project
-          project = import
-            (generatedCargoNix {
-              inherit name;
-              src = ./.;
-            })
-            { inherit pkgs; };
+        rust = pkgs.rust-bin.stable.latest.default;
 
-          # Configuration for the non-Rust dependencies
-          buildInputs = with pkgs; [ ];
-          nativeBuildInputs = with pkgs; [ rustc cargo pkgconfig nixpkgs-fmt ];
-          buildEnvVars = { };
-        in
-        rec {
-          packages.${name} = project.rootCrate.build;
+        naersk-lib = naersk.lib.${system}.override {
+          cargo = rust;
+          rustc = rust;
+        };
 
-          # `nix build`
-          defaultPackage = packages.${name};
+        temps = naersk-lib.buildPackage {
+          pname = name;
+          root = ./.;
+        };
+      in rec {
+        packages.${name} = temps;
+        packages.default = packages.${name};
 
-          # `nix run`
-          apps.${name} = utils.lib.mkApp {
-            inherit name;
-            drv = packages.${name};
-          };
-          defaultApp = apps.${name};
+        apps.${name} = flake-utils.lib.mkApp {
+          inherit name;
+          drv = packages.${name};
+        };
+        apps.default = apps.${name};
 
-          # `nix develop`
-          devShell = pkgs.mkShell
-            {
-              inherit buildInputs nativeBuildInputs;
-              RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-            } // buildEnvVars;
-        }
-      );
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            rust
+            pkgs.rust-analyzer
+          ];
+        };
+      }
+    );
 }
-
